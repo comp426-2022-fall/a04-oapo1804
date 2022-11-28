c0a6d04b52a0da0caa92322580def0aed53893af
Oriana Apostoli <78334888+oapo1804@users.noreply.github.com>
2022-11-28 14:58:46 -0500

    begin

---
+++ server.js
@@ -0,0 +1,22 @@
+import {roll} from "./lib/roll.js";
+import express from "express";
+import minimist from "minimist";
+
+const app = express()
+
+const args = minimist(process.argv.slice(2));
+
+args['port'];
+const port = args.port || 5000
+
+const server = app.listen(port);
+
+
+app.get('/app/', (req, res) => {
+    res.send('200 OK')
+    res.end;
+});
+
+app.post('/app/roll/', (req, res) => {
+    
+});
